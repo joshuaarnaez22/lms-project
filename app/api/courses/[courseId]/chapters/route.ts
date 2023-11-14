a9fca199c23e80d19d89fefba11457b9715e5e3b
joshua <joshuaarnaez22@gmail.com>
2023-11-15 00:02:18 +0800

    fix prisma issue on deployment

--- app/api/courses/[courseId]/chapters/route.ts
+++ app/api/courses/[courseId]/chapters/route.ts
@@ -1,5 +1,6 @@
 import { auth } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function POST(
   req: NextRequest,
