385d3aee4c6869ceae8295cddcb9b88deb47f46c
Joshua <joshuaarnaez22@gmail.com>
2024-01-31 02:57:40 +0800

    ...

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata({
 
   return {
     title: course?.title,
-    metadataBase: new URL("https://lms-project-five.vercel.app"),
+    metadataBase: new URL("https://lms-project-five.vercel.app/"),
     description: "",
     openGraph: {
       images: [
@@ -49,95 +49,96 @@ const CourseChapterIdPage = async ({
 }: {
   params: { chapterId: string; courseId: string };
 }) => {
-  const { userId } = auth();
+  // const { userId } = auth();
 
-  if (!userId) {
-    return redirect("/");
-  }
+  // if (!userId) {
+  //   return redirect("/");
+  // }
 
-  const {
-    userProgress,
-    muxData,
-    chapter,
-    course,
-    attachments,
-    nextChapter,
-    purchase,
-  } = await getChapter({ userId, ...params });
+  // const {
+  //   userProgress,
+  //   muxData,
+  //   chapter,
+  //   course,
+  //   attachments,
+  //   nextChapter,
+  //   purchase,
+  // } = await getChapter({ userId, ...params });
 
-  if (!chapter || !course) {
-    return redirect("/");
-  }
+  // if (!chapter || !course) {
+  //   return redirect("/");
+  // }
 
-  const isLocked = !chapter.isFree && !purchase;
-  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+  // const isLocked = !chapter.isFree && !purchase;
+  // const completeOnEnd = !!purchase && !userProgress?.isCompleted;
 
   return (
-    <div>
-      {userProgress?.isCompleted && (
-        <Banner
-          label="You have already completed this chapter."
-          type="success"
-        />
-      )}
-      {isLocked && (
-        <Banner
-          label="You need to purchase this course to watch this chapter."
-          type="warning"
-        />
-      )}
-      <div className="flex flex-col max-w-4xl mx-auto pb-20">
-        <div className="p-4">
-          <VideoPlayer
-            chapterId={chapter.id}
-            title={chapter.title}
-            courseId={params.courseId}
-            playbackId={muxData?.playbackId!}
-            isLocked={isLocked}
-            completeOnEnd={completeOnEnd}
-            nextChapter={nextChapter?.id}
-          />
-        </div>
-        <div>
-          <div className="flex p-4  flex-col items-center justify-center lg:flex-row lg:justify-between w-full ">
-            <h2 className=" text-2xl font-medium mb-2">{chapter.title}</h2>
-            {purchase ? (
-              <CourseProgressButton
-                chapterId={params.chapterId}
-                courseId={params.courseId}
-                nextChapterId={nextChapter?.id}
-                isComplete={userProgress?.isCompleted}
-              />
-            ) : (
-              <div className="w-full lg:w-auto">
-                <EnrollButton price={course.price!} courseId={course.id} />
-              </div>
-            )}
-          </div>
-        </div>
-        <Separator />
-        <div>
-          <Preview value={chapter.description!} />
-        </div>
-        <div>
-          {!!attachments.length && (
-            <div className="p-4">
-              {attachments.map((attachment) => (
-                <a
-                  href={attachment.url!}
-                  key={attachment.id}
-                  target="_blank"
-                  className="flex items-center p-3 w-full bg-sky-200 border hover:bg-sky-700 transition rounded-md"
-                >
-                  <File className="w-4 h-4" />
-                  <p className=" line-clamp-1">{attachment.name}</p>
-                </a>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+    <h1>hello</h1>
+    // <div>
+    //   {userProgress?.isCompleted && (
+    //     <Banner
+    //       label="You have already completed this chapter."
+    //       type="success"
+    //     />
+    //   )}
+    //   {isLocked && (
+    //     <Banner
+    //       label="You need to purchase this course to watch this chapter."
+    //       type="warning"
+    //     />
+    //   )}
+    //   <div className="flex flex-col max-w-4xl mx-auto pb-20">
+    //     <div className="p-4">
+    //       <VideoPlayer
+    //         chapterId={chapter.id}
+    //         title={chapter.title}
+    //         courseId={params.courseId}
+    //         playbackId={muxData?.playbackId!}
+    //         isLocked={isLocked}
+    //         completeOnEnd={completeOnEnd}
+    //         nextChapter={nextChapter?.id}
+    //       />
+    //     </div>
+    //     <div>
+    //       <div className="flex p-4  flex-col items-center justify-center lg:flex-row lg:justify-between w-full ">
+    //         <h2 className=" text-2xl font-medium mb-2">{chapter.title}</h2>
+    //         {purchase ? (
+    //           <CourseProgressButton
+    //             chapterId={params.chapterId}
+    //             courseId={params.courseId}
+    //             nextChapterId={nextChapter?.id}
+    //             isComplete={userProgress?.isCompleted}
+    //           />
+    //         ) : (
+    //           <div className="w-full lg:w-auto">
+    //             <EnrollButton price={course.price!} courseId={course.id} />
+    //           </div>
+    //         )}
+    //       </div>
+    //     </div>
+    //     <Separator />
+    //     <div>
+    //       <Preview value={chapter.description!} />
+    //     </div>
+    //     <div>
+    //       {!!attachments.length && (
+    //         <div className="p-4">
+    //           {attachments.map((attachment) => (
+    //             <a
+    //               href={attachment.url!}
+    //               key={attachment.id}
+    //               target="_blank"
+    //               className="flex items-center p-3 w-full bg-sky-200 border hover:bg-sky-700 transition rounded-md"
+    //             >
+    //               <File className="w-4 h-4" />
+    //               <p className=" line-clamp-1">{attachment.name}</p>
+    //             </a>
+    //           ))}
+    //         </div>
+    //       )}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
