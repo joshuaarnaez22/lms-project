5f2f7c7e2515bcdf2624b7cda2498a06d4f8e7e1
joshuaarnaez22 <joshuaarnaez22@gmail.com>
2024-05-17 22:04:51 +0800

    fix image name 1

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 const Logo = () => {
-  return <Image height={90} width={90} alt="logo" src="/logo.png" />;
+  return <Image height={70} width={70} alt="logo" src="/logo.png" />;
 };
 
 export default Logo;
